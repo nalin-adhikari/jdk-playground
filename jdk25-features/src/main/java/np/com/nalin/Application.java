8045d441d530672c1995623fd396fec941b3cb91
Nalin Adhikari <nalinadhikariofficial@gmail.com>
2025-10-09 01:15:38 +1100

    Project Init, module created: jdk25-features

---
+++ jdk25-features/src/main/java/np/com/nalin/Application.java
@@ -0,0 +1,8 @@
+package main.java.np.com.nalin;
+
+public class Application {
+    public static void main(String[] args) {
+        // Initialize the application
+        System.out.println("JDK 25 Features Application Started");
+    }
+}
---
+++ jdk25-features/src/main/java/np/com/nalin/features/StringTemplates.java
@@ -0,0 +1,8 @@
+package np.com.nalin.features;
+
+public class StringTemplates {
+
+    public static String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+}
